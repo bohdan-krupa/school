e57e6f88e43376c0327734f1e4e459b5ee7f0ebc
Bodya <bodya.save.dev@gmail.com>
2019-03-27 20:29:38 +0200

    update

--- src/js/map.js
+++ src/js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
 	let myLatLng = { lat: 49.800498, lng: 24.054980 }
 
-	let map = new google.maps.Map(document.querySelector(".map"), {
+	let map = new google.maps.Map(document.querySelector('.map'), {
 		center: myLatLng,
 		// mapTypeId: "satellite",
 		zoom: 15
