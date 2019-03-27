c43c71ef2ee080b346160124fee110d66af771b7
Bodya <bodya.save.dev@gmail.com>
2019-03-27 21:31:20 +0200

    update

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -22,7 +22,6 @@ Vue.component('my-header', {
 			}
 		}
 	},
-
 	template: `<div class="header">
 		<router-link to="/" class="home parent" v-bind:class="{ active: active.home }">ГОЛОВНА</router-link>
 		<router-link to="/events" class="events parent" v-bind:class="{ active: active.events }">Події</router-link>
