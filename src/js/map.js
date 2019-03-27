67c78b730b8089dc9e163a8729ed57c079b8e7f3
Bodya <bodya.save.dev@gmail.com>
2019-03-27 20:28:53 +0200

    update

--- src/js/map.js
+++ src/js/map.js
@@ -1,15 +1,15 @@
 function initMap() {
-	var myLatLng = {lat: 49.800498, lng: 24.054980};
+	let myLatLng = { lat: 49.800498, lng: 24.054980 }
 
-	var map = new google.maps.Map(document.querySelector(".map"), {
+	let map = new google.maps.Map(document.querySelector(".map"), {
 		center: myLatLng,
 		// mapTypeId: "satellite",
 		zoom: 15
-	});
+	})
 
-	var marker = new google.maps.Marker({
+	let marker = new google.maps.Marker({
 		map: map,
 		position: myLatLng,
 		title: "Школа №96"
-	});
+	})
 }
