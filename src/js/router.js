b8d0dfc7cc1df3bae1f1cb1e700e7e46434c1f6f
Bodya <bodya.save.dev@gmail.com>
2019-03-27 07:35:07 +0200

    update

--- src/js/router.js
+++ src/js/router.js
@@ -1,6 +1,6 @@
 let router = new VueRouter({
 	routes: [
 		{ path: '/', component: Home },
-		{ path: 'events', component: EventsContainer }
+		{ path: '/events/', component: EventsContainer }
 	]
 })
