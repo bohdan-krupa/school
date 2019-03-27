ee5aedcb067ac6170f351d3a06e0fd29a8a378e5
Bodya <bodya.save.dev@gmail.com>
2019-03-27 07:37:40 +0200

    update

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -10,5 +10,3 @@ Vue.component('my-footer', {
         <a href="https://www.facebook.com/%D0%9D%D0%B0%D1%88%D0%B0-%D1%83%D0%BB%D1%8E%D0%B1%D0%BB%D0%B5%D0%BD%D0%B0-96-%D1%88%D0%BA%D0%BE%D0%BB%D0%B0-%D0%BC-%D0%9B%D1%8C%D0%B2%D1%96%D0%B2-320650714766394/" class="fa fa-facebook"></a>
     </footer>`
 });
-
-new Vue({ el: 'my-footer' });
