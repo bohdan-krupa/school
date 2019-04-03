dc41df5da9d14376f6934c0bc1c5af1af3f675a8
Bodya <bodya.save.dev@gmail.com>
2019-04-03 23:41:34 +0300

    update

--- src/js/getData.js
+++ src/js/getData.js
@@ -2,7 +2,5 @@ let databaseRef = firebase.database().ref()
 let numberOfStudents = databaseRef.child('numberOfStudents')
 
 numberOfStudents.once('value').then(snap => {
-	console.log(snap.val())
 	app.numberOfStudents = snap.val()
 })
-console.log(app.numberOfStudents)
