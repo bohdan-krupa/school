b41597701a579decc8e3a30b534d6cf2e80f2f22
Bodya <bodya.save.dev@gmail.com>
2019-04-04 01:16:34 +0300

    update

---
+++ src/js/components/Admin.js
@@ -0,0 +1,17 @@
+let AdminHome = Vue.component('admin-home', {
+	data: () => {
+		return {
+			actions: [
+				{ name: 'Додати подію', link: '/add-event/' },
+				{ name: 'Редагувати подію', link: '/edit-event/' },
+				{ name: 'Видалити подію', link: '/delete-event/' },
+				{ name: 'Кількість учнів', link: '/number-of-students/' },
+				{ name: 'Кількість вчителів', link: '/number-of-teachers/' },
+				{ name: 'Редагувати канікули', link: '/edit-holidays/' }
+			]
+		}
+	},
+	template: `<div class="admin-container">
+		<router-link v-for="action in actions" :to="action.link">{{ action.name }}</router-link>
+	</div>`
+})
