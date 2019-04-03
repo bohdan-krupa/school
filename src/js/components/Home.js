1d5891e8c7d470ddd6d57418e83378a6f99e88a0
Bodya <bodya.save.dev@gmail.com>
2019-04-03 23:32:02 +0300

    update

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -228,7 +228,7 @@ Vue.component('main-events', {
 let LiveNumbers = Vue.component('live-numbers', {
 	data: () => {
 		return {
-			// numbersOfStudents: 'fs',
+			numbersOfStudents: 'fs',
 			numbersOfTeachers: null
 		}
 	},
