4ab70d436d117689d97a93a95f8648295482d37d
Bodya <bodya.save.dev@gmail.com>
2019-04-03 19:46:43 +0300

    update

---
+++ src/js/components/Preloader.js
@@ -0,0 +1,3 @@
+Vue.component('preloader', {
+	template: ``
+})
---
+++ src/js/routerForAdmin.js
@@ -0,0 +1,7 @@
+let routerForAdmin = new VueRouter({
+	routes: [
+		{ path: '/', component:AdminMain },
+		// { path: '/events/', component: EventsContainer },
+		// { path: '/contact/', component: ContactContainer }
+	]
+})
